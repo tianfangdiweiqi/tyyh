04f47ea2a257f2cc75533a10f16533d816b70d49
tianfangdiweiqi <15513039176@163.com>
2017-12-23 15:01:56 +0800

    01

---
+++ s1/src/test/test01.java
@@ -0,0 +1,27 @@
+package test;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		/*Integer i=Integer.valueOf(10);
+		Integer j=10;
+		System.out.println(i==j);
+		
+		Integer a=128;
+		Integer b=128;
+		System.out.println(a==b);
+		
+		int k=10;
+		System.out.println(i==k);
+		int kk=128;
+		System.out.println(kk==a);
+		
+		Integer m=new  Integer(10);
+		Integer n=new Integer(10);
+		System.out.println(m==n);*/
+		Integer a = 129;
+		int c = a;
+		Integer b = 129;
+		System.out.println(c == b);
+	}
+}
